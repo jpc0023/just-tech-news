aab98ab5f75192ac98a623546703162cdf4226e7
jpc0023 <91299930+jpc0023@users.noreply.github.com>
2022-03-14 19:45:11 -0400

    comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 // get all users //
 router.get('/', (req, res)=>{
     console.log('===============');
     Post.findAll({
+        order: [['created_at', 'DESC']],
         attributes: [
             'id', 
             'post_url', 
@@ -16,8 +17,15 @@ router.get('/', (req, res)=>{
                 'vote_count'
             ]
         ],
-        order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
