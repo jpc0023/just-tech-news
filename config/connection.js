99b2d72c521738b9dd3373f7c3c17fa03ac902b4
jpc0023 <91299930+jpc0023@users.noreply.github.com>
2022-03-14 10:02:27 -0500

    bcrypt hashing

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our database //
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'Jcro$21083', {
+const sequelize = new Sequelize('just_tech_news_db', 'test', 'test', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
